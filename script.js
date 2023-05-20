0fe5d062536259bd348fd602553f14be23ddf72f
Muthuselvamramesh <muthuselvam@aa2akhtech.in>
2023-05-20 14:43:22 +0530

    Changed styles and code format

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-function openPopUp () {
+function openPopUp() {
   popup.classList.add("open-popup");
 }
 
-function displayLink () {
-  const num = document.getElementById('number').value;
-  const msg = document.getElementById('message').value;
+function displayLink() {
+  const num = document.getElementById("number").value;
+  const msg = document.getElementById("message").value;
   const url = "https://wa.me/";
   const finalUrl = `${url}${num}?text=${msg}`;
-  document.getElementById('linkDisplay').innerHTML = finalUrl;
-  const openWhat = document.getElementById('openWhatsapp');
-  openWhat.onclick = function (){
+  document.getElementById("displayLink").innerHTML = finalUrl;
+  const openWhatsapp = document.getElementById("open-whatsapp-btn");
+  openWhatsapp.onclick = function () {
     window.location.href = finalUrl;
-  }
-
+  };
 }
 
-function createLink () {
+function createLink() {
   openPopUp();
   displayLink();
 }
 
-const copybtn = document.getElementById("copyBtn");
+const copybtn = document.getElementById("copy-text-btn");
 
 copybtn.onclick = function () {
-  var copyTxt = document.getElementById("linkDisplay");
+  var copyTxt = document.getElementById("displayLink");
   copyTxt.select();
   navigator.clipboard.writeText(copyTxt.value);
-}
+};
 
-const qrInput = document.querySelector(".formTwo textarea");
-const qrImg = document.querySelector(".formTwo img")
-generateBtn = home.querySelector(".home button");
+const qrInput = document.querySelector(".link-generate-form textarea");
+const qrImg = document.querySelector(".link-generate-form img");
+generateBtn = document.querySelector(".homepage button");
 
 generateBtn.addEventListener("click", () => {
   const qrValue = qrInput.value;
